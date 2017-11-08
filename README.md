1bbab4625432123668ede44e58b483fee649c470
Alex Lakatos <alex.lakatos.dev@gmail.com>
2017-11-08 10:47:25 +0000

    boilerplate angular app and node server

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # nexmo-angular-sms
-SMS Notifications in the browser with Angular
+1. Download [ngrok](https://ngrok.com/download)
+2. `$ ./ngrok http 3000`
+3. `$ npm install`
+4. `$ npm run server`
+5. `$ ng serve`
---
+++ server.js
@@ -0,0 +1,10 @@
+'use strict';
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+const server = app.listen(3000, () => {
+    console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+});
